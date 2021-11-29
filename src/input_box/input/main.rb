cba27675f0b7cecf6c0284af390c99e16b12f370
Mario Chabot <marchab@me.com>
2021-11-29 14:44:42 -0500

    Add files via upload

---
+++ src/input_box/input/main.rb
@@ -0,0 +1,11 @@
+require 'sketchup.rb'
+
+  require 'F3D_Lib/debug'
+  require 'F3D_Lib/html_ui_input'
+  require 'F3D_Lib/input/checkbox'
+  require 'F3D_Lib/input/dropdown'
+  require 'F3D_Lib/input/listbox'
+  require 'F3D_Lib/input/textbox'
+  require 'F3D_Lib/html_ui_message'
+  require 'F3D_Lib/input/message'
+
---
+++ src/input_box/input/message.rb
@@ -0,0 +1,15 @@
+ require 'F3D_Lib/input/input'
+
+module F3D_HtmlInput
+  module HtmlUI
+
+    class Message < Input
+
+       def initialize(label)
+        super(label: label)
+      end
+
+    end # class
+
+  end # module
+end # module
